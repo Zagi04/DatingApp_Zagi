600976701c66fd424c25f9afada2be6892843e89
Zahorán Áron <zahoranaron2004@gmail.com>
2024-02-07 22:14:36 +0100

    End of section 7

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using System.Text;
 using API.Data;
 using API.Extension;
 using API.Interfaces;
+using API.Middleware;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
@@ -17,6 +18,8 @@ builder.Services.AddIdentityService(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.UseAuthentication(); //Van-e érvényes tokened
